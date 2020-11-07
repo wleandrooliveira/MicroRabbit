7555d5e2b549285d6f4c00fcdf6aef5e5de8acff
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2020-11-07 03:36:40 -0300

    Refactor Controller and Other Microservices

--- MicroRabbit/MicroRabbit.Infra.Ioc/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.Ioc/DependencyContainer.cs
@@ -12,6 +12,11 @@ using MicroRabbit.Banking.Data.Context;
 using MediatR;
 using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.CommandHandlers;
+using MicroRabbit.Transfer.Application.Services;
+using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Data.Repository;
+using MicroRabbit.Transfer.Domain.Interfaces;
 
 namespace MicroRabbit.Infra.Ioc
 {
@@ -27,10 +32,13 @@ namespace MicroRabbit.Infra.Ioc
 
             //Application Services
             services.AddTransient<IAccountService, AccountService>();
+            services.AddTransient<ITransferService, TransferService>();
 
             //Data
             services.AddTransient<IAccountRepository, AccountRepository > ();
+            services.AddTransient<ITransferRepository, TransferRepository> ();
             services.AddTransient<BankingDbContext>();
+            services.AddTransient<TransferDbContext>();
         }
     }
 }
